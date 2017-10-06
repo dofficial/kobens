c440b5725303d5a20be3e2e6fa6260b70eb9b693
Darren Felton <dfelton@amsi-corp.com>
2017-10-05 21:33:51 -0500

    Working on those order events. Got shit going a bit.

--- app/code/Kobens/Gemini/Api/V1/WebSocket/OrderData/OrderEvents.php
+++ app/code/Kobens/Gemini/Api/V1/WebSocket/OrderData/OrderEvents.php
@@ -19,9 +19,9 @@ class OrderEvents
     protected $scopeConfig;
 
     /**
-     * @var \Magento\Framework\Json\DecoderInterface
+     * @var \Magento\Framework\Encryption\EncryptorInterface
      */
-    protected $jsonDecoder;
+    protected $encryptorInterface;
 
     /**
      * @var OutputInterface
@@ -34,6 +34,11 @@ class OrderEvents
     protected $filters = [];
 
     /**
+     * @var \Kobens\Gemini\Helper\Nonce
+     */
+    protected $nonce;
+
+    /**
      * @var string
      */
     protected $secretApiKey;
@@ -44,18 +49,95 @@ class OrderEvents
     protected $publicApiKey;
 
     /**
+     * @var string
+     */
+    protected $websocketUrl;
+
+    /**
+     * @var \Kobens\Gemini\Model\Cache
+     */
+    protected $cache;
+
+    /**
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
      * @param \Magento\Framework\Json\DecoderInterface $jsonDecoder
      * @param array $filters
      */
     public function __construct(
+        \Kobens\Gemini\Model\Cache $cache,
+        \Kobens\Gemini\Helper\Nonce $nonce,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
-        \Magento\Framework\Json\DecoderInterface $jsonDecoder,
+        \Magento\Framework\Encryption\EncryptorInterface $encryptorInterface,
         $filters = []
     ) {
+        $this->encryptorInterface = $encryptorInterface;
         $this->filters = $filters;
-        $this->jsonDecoder = $jsonDecoder;
+        $this->nonce = $nonce;
         $this->scopeConfig = $scopeConfig;
+    }
+
+    public function openOrderBook()
+    {
+        $this->socketSequence = 0;
+        $loop = \React\EventLoop\Factory::create();
+        $connector = new \Ratchet\Client\Connector($loop);
+
+        $connector($this->getWebSocketUrl(), [], $this->getHeaders())->then(
+            function (\Ratchet\Client\WebSocket $conn) {
+                $conn->on('message', function(\Ratchet\RFC6455\Messaging\MessageInterface $msg) use ($conn) {
+                    $msg = json_decode($msg);
+                    // Just trying to get first message
+                    \Zend_Debug::dump($msg);
+                });
+
+                $conn->on('close', function($code = null, $reason = null) {
+                    $this->outputMsg("Connection Closed.\n");
+                });
+            },
+            function (\Exception $e) use ($loop) {
+                $loop->stop();
+                $this->outputMsg("\nException Thrown: {$e->getMessage()}\n");
+                \Zend_Debug::dump($e->getTraceAsString());
+            }
+        );
+        $loop->run();
+    }
+
+    protected function getWebSocketUrl()
+    {
+        if (!$this->websocketUrl) {
+            $url = self::WEBSOCKET_URL;
+            // TODO: apply filters
+            $this->websocketUrl = $url;
+        }
+        return $this->websocketUrl;
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getHeaders()
+    {
+        $payload = $this->getPayload();
+        $headers = [
+            'X-GEMINI-APIKEY' => $this->getPublicApiKey(),
+            'X-GEMINI-PAYLOAD' => $payload,
+            'X-GEMINI-SIGNATURE' => $this->getSignature($payload)
+        ];
+        return $headers;
+    }
+
+    protected function getPayload()
+    {
+        return base64_encode(json_encode([
+            'request' => '/v1/order/events',
+            'nonce' => $this->nonce->getNonce()
+        ]));
+    }
+
+    protected function getSignature($payload)
+    {
+        return hash_hmac('sha384', $payload, $this->getSecretApiKey());
     }
 
     /**
@@ -64,9 +146,10 @@ class OrderEvents
     public function setOutput(OutputInterface $output)
     {
         $this->outputInterface = $output;
+        return $this;
     }
 
-    protected function outputMsg($message)
+    public function outputMsg($message)
     {
         if ($this->outputInterface) {
             $this->outputInterface->write($message);
@@ -84,8 +167,7 @@ class OrderEvents
             if (!$key) {
                 throw new \Kobens\Core\Exception\NoApiKeyException('Gemini Trader Public API Key not set.');
             }
-            // TODO: Throw invalid API key exception if decode fails
-            $key = $this->jsonDecoder->decode($key);
+            $key = $this->encryptorInterface->decrypt($key);
             $this->publicApiKey = $key;
         }
         return $this->publicApiKey;
@@ -102,9 +184,7 @@ class OrderEvents
             if (!$key) {
                 throw new \Kobens\Core\Exception\NoApiKeyException('Gemini Trader Secret API Key not set.');
             }
-            // TODO: Throw invalid API key exception if decode fails
-            $key = $this->jsonDecoder->decode($key);
-            $this->secretApiKey = $key;
+            $this->secretApiKey = $this->encryptorInterface->decrypt($key);
         }
         return $this->secretApiKey;
     }
